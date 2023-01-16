f8c579bc8a684a0052c63d96ea8532b8529b0ba6
prolegion2021 <prolegion2021@gmail.com>
2023-01-17 00:23:29 +0200

    Fix contact route

--- shadow-app/shadow_api.py
+++ shadow-app/shadow_api.py
@@ -50,10 +50,12 @@ def reddit_user(username):
 @app.route('/')
 def index():
     return render_template('index.html')
-#
-# @app.route('/contacts')
-# def index():
-#     return render_template('contacts.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contacts.html')
+
 
 if __name__ == '__main__':
     print("Starting Prod API server...")
